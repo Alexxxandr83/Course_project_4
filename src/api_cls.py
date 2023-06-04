e633c631bb412613290b6a4f6ec49ddd456f1bbb
Alexandp L <alexandp83@mail.ru>
2023-06-04 19:39:20 +0300

    Создали класс получения вакансий с HH.ru

--- src/api_cls.py
+++ src/api_cls.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import requests
 
 
 class AbstractApiClass(ABC):
@@ -7,3 +8,34 @@ class AbstractApiClass(ABC):
     @abstractmethod
     def get_vacancies(self):
         pass
+
+
+class HH_API(AbstractApiClass):
+    """Получаем данные с hh.ru"""
+
+    def get_vacancies(self):
+
+        url = 'https://api.hh.ru/vacancies'
+        params = {'area': 1, 'text': 'python',
+                  'per_page': 10}
+
+        response = requests.get(url, params=params)
+
+        if response.status_code == 200:
+            data = response.json()
+            vacancies = []
+            for item in data['items']:
+                salary = item.get('salary', {})
+                vacancies.append(
+                    Vacancy(
+                        name=item['name'],
+                        url=item['url'],
+                        description=item['snippet']['requirement'],
+                        salary_to=salary.get('to') if salary is not None else None,
+                        salary_from=salary.get('from') if salary is not None else None
+                    )
+                )
+            return vacancies
+        else:
+            print('Ошибка при запросе данных о вакансиях')
+            return None
